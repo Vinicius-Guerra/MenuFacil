81eec76a4230478d61a91d28207a8a376ac1fadd
Vinicius Guerra <vini.fms@hotmail.com>
2024-06-10 19:20:23 -0300

    Update service and Configs vars jwt

--- src/schemas/restaurant.schema.ts
+++ src/schemas/restaurant.schema.ts
@@ -7,5 +7,22 @@ export const restaurantSchema = z.object({
     email: z.string().min(1).email(),
     password: z.string().min(1),
 });
-
 export type TRestaurant = z.infer<typeof restaurantSchema>;
+
+export const  restaurantRegisterBodySchema = restaurantSchema.omit({ id: true, description: true });
+export type TRestaurantRegisterBody = z.infer<typeof restaurantRegisterBodySchema>;
+
+export const restaurantLoginBodySchema = restaurantSchema.pick({ email: true, password: true });
+export type TRestaurantLoginBody = z.infer<typeof restaurantLoginBodySchema>;
+
+export const restaurantUpdateBodySchema = restaurantSchema.pick({ description: true });
+export type TRestaurantUpdateBody = z.infer<typeof restaurantUpdateBodySchema>;
+
+
+export const restaurantReturnSchema = restaurantSchema.omit({ password: true });
+export type TRestaurantReturn = z.infer<typeof restaurantReturnSchema>;
+
+export type TRestaurantLoginReturn = {
+    accessToken: string,
+    restaurant: TRestaurantReturn
+};
---
+++ src/services/restaurant.service.ts
@@ -0,0 +1,59 @@
+import { prisma } from "../database/prisma";
+import { AppError } from "../errors/appError";
+import { TRestaurantLoginBody, TRestaurantLoginReturn, TRestaurantRegisterBody, TRestaurantReturn, TRestaurantUpdateBody, restaurantReturnSchema } from "../schemas/restaurant.schema";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export class RestaurantService {
+    async register(body: TRestaurantRegisterBody): Promise<TRestaurantReturn> {
+        const existingRestaurant = await prisma.restaurant.findFirst({ where: { email: body.email }});
+
+        if(existingRestaurant) {
+            throw new AppError("This email is already registered", 403);
+        }
+
+        const hashPassword = await bcrypt.hash(body.password, 10);
+        
+        const newRestaurantData = {
+            ...body,
+            password: hashPassword
+        };
+
+        const restaurant = await prisma.restaurant.create({ data: newRestaurantData });
+
+        return restaurantReturnSchema.parse(restaurant);
+    };
+
+    async login(body: TRestaurantLoginBody): Promise<TRestaurantLoginReturn> {
+        const restaurant = await prisma.restaurant.findFirst({ where: { email: body.email }});
+
+        if(!restaurant) {
+            throw new AppError("Restaurant not registered", 403);
+        };
+
+        const comparePassword = await bcrypt.compare(body.password, restaurant.password);
+
+        if(!comparePassword) {
+            throw new AppError("Email and password doesn't match", 401);
+        };
+        
+        const token = jwt.sign({ id: restaurant.id }, process.env.JWT_SECRET as string);
+
+        return {
+            accessToken: token,
+            restaurant: restaurantReturnSchema.parse(restaurant),
+        };
+    };
+
+    async update(body: TRestaurantUpdateBody, restaurantId: string): Promise<TRestaurantReturn> {
+        const restaurant = await prisma.restaurant.update({ where: { id: restaurantId }, data: body });
+
+        return restaurantReturnSchema.parse(restaurant);
+    };
+
+    async getRestaurant(restaurantId: string): Promise<TRestaurantReturn> {
+        const restaurant = await prisma.restaurant.findFirst({ where: { id: restaurantId }});
+
+        return restaurantReturnSchema.parse(restaurant);
+    }
+}

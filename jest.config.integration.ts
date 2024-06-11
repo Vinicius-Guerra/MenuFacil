8e3f4bb909765d27467f414a86b281a57f6f1943
Vinicius Guerra <vini.fms@hotmail.com>
2024-06-11 16:59:22 -0300

    Jest init config Units and integrations

--- jest.config.integration.ts
+++ jest.config.integration.ts
@@ -4,5 +4,5 @@ export default {
     preset: "ts-jest",
     testEnvironment: "node",
     testMatch: ["**/__tests__/integrations/**/*.[jt]s?(x)"],
-    //setupFilesAfterEnv: ["./src/__tests__/__mocks__/prisma.ts", "./src/__tests__/utils/reflectMetadata.ts"]
+    setupFilesAfterEnv: ["./src/__tests__/utils/clearDatabase.ts"]
 };

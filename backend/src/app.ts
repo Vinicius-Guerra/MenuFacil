e7df80f6168c3c369340ee810f76c3a1b0dae320
Vinicius Guerra <vini.fms@hotmail.com>
2024-08-06 17:11:43 -0300

    Alter for deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
+import "dotenv/config";
 import "express-async-errors";
 import "reflect-metadata";
-import "dotenv/config";
 import cors from "cors";
 import express, { json } from "express";
 import helmet from "helmet";
@@ -13,7 +13,7 @@ export const app = express();
 
 //security
 const corsOption = {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
 };
 
